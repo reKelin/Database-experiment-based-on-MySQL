808ec535c909b904dfb670fb480c914c1127ea12
Kelin <1311740140@qq.com>
2022-04-29 14:15:15 +0800

    update 11.2

--- 11. MySQL - 备份+日志：介质故障与数据库恢复/2_1.sh
+++ 11. MySQL - 备份+日志：介质故障与数据库恢复/2_1.sh
@@ -1,2 +1,1 @@
-mysqldump -h127.0.0.1 -uroot --databases train > train_bak.sql;
-mysqladmin flush-logs
+mysqldump -h127.0.0.1 -uroot --flush-logs --databases train > train_bak.sql;
--- 11. MySQL - 备份+日志：介质故障与数据库恢复/2_2.sh
+++ 11. MySQL - 备份+日志：介质故障与数据库恢复/2_2.sh
@@ -1,2 +1,2 @@
 mysql -h127.0.0.1 -uroot < train_bak.sql;
-mysqlbinlog --no-defaults --disable-log-bin log/binlog.000018 | mysql -uroot;
+mysqlbinlog --no-defaults log/binlog.000018 | mysql -uroot;
