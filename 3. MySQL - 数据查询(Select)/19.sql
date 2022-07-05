4a6b45b59f0add384d3a26f4ad6f9b54a8d1484c
Kelin <1311740140@qq.com>
2022-07-05 17:10:34 +0800

    change 'case' to 'if'

--- 3. MySQL - 数据查询(Select)/19.sql
+++ 3. MySQL - 数据查询(Select)/19.sql
@@ -1,20 +1,22 @@
 select
     wk week_of_trading,
-    sum(case dayId when 0 then amount end) Monday,
-    sum(case dayId when 1 then amount end) Tuesday,
-    sum(case dayId when 2 then amount end) Wednesday,
-    sum(case dayId when 3 then amount end) Thursday,
-    sum(case dayId when 4 then amount end) Friday
+    sum(if(dayId = 0, amount, null)) Monday,
+    sum(if(dayId = 1, amount, null)) Tuesday,
+    sum(if(dayId = 2, amount, null)) Wednesday,
+    sum(if(dayId = 3, amount, null)) Thursday,
+    sum(if(dayId = 4, amount, null)) Friday
 from (
     select
         week(pro_purchase_time) - 5 wk,
         weekday(pro_purchase_time) dayId,
         sum(pro_quantity * f_amount) amount
-    from property
-    join fund
-    on pro_pif_id = f_id
-    where pro_purchase_time like "2022-02-%"
-    and pro_type = 3
+    from
+        property
+        join fund
+        on pro_pif_id = f_id
+    where
+        pro_purchase_time like "2022-02-%"
+        and pro_type = 3
     group by pro_purchase_time
 ) t
 group by wk;
