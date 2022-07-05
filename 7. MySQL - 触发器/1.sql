ee6371c81b009aa08bdc1b53a2654605a9d5bda9
Kelin <1311740140@qq.com>
2022-07-05 18:23:05 +0800

    Simplified code

--- 7. MySQL - 触发器/1.sql
+++ 7. MySQL - 触发器/1.sql
@@ -7,9 +7,6 @@ BEGIN
     declare tp int default new.pro_type;
     declare id int default new.pro_pif_id;
     declare msg varchar(50);
-    if tp not in (1, 2, 3) then
-        set msg = concat("type ", tp, " is illegal!");
-    end if;
     if tp = 1 then
         if id not in (select p_id from finances_product) then
             set msg = concat("finances product #", id, " not found!");
@@ -22,10 +19,11 @@ BEGIN
         if id not in (select f_id from fund) then
             set msg = concat("fund #", id, " not found!");
         end if;
+    else
+        set msg = concat("type ", tp, " is illegal!");
     end if;
     if msg is not null then
         signal sqlstate "45000" set message_text = msg;
     end if;
 END$$
- 
 delimiter ;
